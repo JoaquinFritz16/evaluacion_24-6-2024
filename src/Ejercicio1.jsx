0b40012df29efb8a73f3a89a02d141563a3d3ca5
Joaquin Ignacio Fritz Carrizo <d48125437@alumnos.itr.edu.ar>
2024-06-24 15:28:01 -0300

    Ejercicio 1

--- src/Ejercicio1.jsx
+++ src/Ejercicio1.jsx
@@ -1,11 +1,22 @@
+import {useState} from "react"
 export default function Ejercicio1() {
+  const [lista, Setlista] = useState([])
 
+  const handleSelect = (e) => {
+    Setlista(e.target.value)
+    window.alert(lista)
+  }
   return (
     <div>
       <h2>Ejercicio 1: Lista desplegable</h2>
 
       <label>Seleccione una fruta: </label>
-      {/* Crear lista de selección */}
+      <select onChange={handleSelect}>
+        <option value="manzana">Manzana</option>
+        <option value="pera">Pera</option>
+        <option value="naranja">Naranja</option>
+        <option value="mandarina">Mandarina</option>
+      </select>
 
     </div>
   )
